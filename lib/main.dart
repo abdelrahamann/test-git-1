3e9dab7951d89ea32dfd967d9ece72edf78345a9
G3 <awwelk123456@gmail.com>
2023-09-20 17:38:33 +0300

    initial project . added empty home screen .

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:assimentt1/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(myApp());
+}
+
+class myApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
